4079d8bec98d30e28e47f6afcee787514f0e020f
1gram4 <1147854543@qq.com>
2019-06-18 12:33:39 +0800

    Initial commit

---
+++ src/main/java/com/pao/shuical/ShuicalApplication.java
@@ -0,0 +1,13 @@
+package com.pao.shuical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShuicalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pao/shuical/ShuicalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pao.shuical;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShuicalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
